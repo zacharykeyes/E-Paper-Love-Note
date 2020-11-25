b1de155f9581ba4c99104d97ff0b16d27146c727
Jamie Bernard <66458928+LazerGerraldo@users.noreply.github.com>
2020-11-25 13:05:08 -0800

    na

--- myTest.py
+++ myTest.py
@@ -35,6 +35,9 @@ try:
     HRYimage = Image.new('1', (epd.height, epd.width), 255)  # 298*126  ryimage: red or yellow image  
     drawblack = ImageDraw.Draw(HBlackimage)
     drawry = ImageDraw.Draw(HRYimage)
+    time.sleep(2)
+
+
     drawblack.text((10, 0), 'hello world', font = font24, fill = 0)
     drawblack.text((10, 20), '2.9inch e-Paper bc', font = font24, fill = 0)
     drawblack.text((150, 0), u'微雪电子', font = font24, fill = 0)    
@@ -58,7 +61,7 @@ try:
     
     drawblack.text((2, 0), 'hello world', font = font18, fill = 0)
     drawblack.text((2, 20), '2.9inch epd bc', font = font18, fill = 0)
-    drawblack.text((20, 50), u'Erwin Industries', font = font18, fill = 0)
+    drawblack.text((20, 50), u'Erwin Indc', font = font18, fill = 0)
     drawblack.line((10, 90, 60, 140), fill = 0)
     drawblack.line((60, 90, 10, 140), fill = 0)
     drawblack.rectangle((10, 90, 60, 140), outline = 0)
