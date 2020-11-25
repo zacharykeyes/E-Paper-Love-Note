08537fb59a828f06957c869af39fdb9dc1e777de
Jamie Bernard <66458928+LazerGerraldo@users.noreply.github.com>
2020-11-25 13:22:18 -0800

    Just Test One Output Horizontal

--- myTest.py
+++ myTest.py
@@ -50,28 +50,28 @@ try:
     drawry.rectangle((80, 50, 130, 100), fill = 0)
     drawry.chord((200, 50, 250, 100), 0, 360, fill = 0)
     epd.display(epd.getbuffer(HBlackimage), epd.getbuffer(HRYimage))
-    time.sleep(2)
+    time.sleep(10)
     
-    # Drawing on the Vertical image
-    logging.info("2.Drawing on the Vertical image...")
-    LBlackimage = Image.new('1', (epd.width, epd.height), 255)  # 126*298
-    LRYimage = Image.new('1', (epd.width, epd.height), 255)  # 126*298
-    drawblack = ImageDraw.Draw(LBlackimage)
-    drawry = ImageDraw.Draw(LRYimage)
+    # # Drawing on the Vertical image
+    # logging.info("2.Drawing on the Vertical image...")
+    # LBlackimage = Image.new('1', (epd.width, epd.height), 255)  # 126*298
+    # LRYimage = Image.new('1', (epd.width, epd.height), 255)  # 126*298
+    # drawblack = ImageDraw.Draw(LBlackimage)
+    # drawry = ImageDraw.Draw(LRYimage)
     
-    drawblack.text((2, 0), 'hello world', font = font18, fill = 0)
-    drawblack.text((2, 20), '2.9inch epd bc', font = font18, fill = 0)
-    drawblack.text((20, 50), u'Erwin Indc', font = font18, fill = 0)
-    drawblack.line((10, 90, 60, 140), fill = 0)
-    drawblack.line((60, 90, 10, 140), fill = 0)
-    drawblack.rectangle((10, 90, 60, 140), outline = 0)
-    drawry.line((95, 90, 95, 140), fill = 0)
-    drawry.line((70, 115, 120, 115), fill = 0)
-    drawry.arc((70, 90, 120, 140), 0, 360, fill = 0)
-    drawry.rectangle((10, 150, 60, 200), fill = 0)
-    drawry.chord((70, 150, 120, 200), 0, 360, fill = 0)
-    epd.display(epd.getbuffer(LBlackimage), epd.getbuffer(LRYimage))
-    time.sleep(2)
+    # drawblack.text((2, 0), 'hello world', font = font18, fill = 0)
+    # drawblack.text((2, 20), '2.9inch epd bc', font = font18, fill = 0)
+    # drawblack.text((20, 50), u'Erwin Indc', font = font18, fill = 0)
+    # drawblack.line((10, 90, 60, 140), fill = 0)
+    # drawblack.line((60, 90, 10, 140), fill = 0)
+    # drawblack.rectangle((10, 90, 60, 140), outline = 0)
+    # drawry.line((95, 90, 95, 140), fill = 0)
+    # drawry.line((70, 115, 120, 115), fill = 0)
+    # drawry.arc((70, 90, 120, 140), 0, 360, fill = 0)
+    # drawry.rectangle((10, 150, 60, 200), fill = 0)
+    # drawry.chord((70, 150, 120, 200), 0, 360, fill = 0)
+    # epd.display(epd.getbuffer(LBlackimage), epd.getbuffer(LRYimage))
+    # time.sleep(2)
     
     # logging.info("3.read bmp file")
     # HBlackimage = Image.open(os.path.join(picdir, '2in9bc-b.bmp'))
@@ -86,9 +86,9 @@ try:
     # blackimage1.paste(newimage, (50,10))    
     # epd.display(epd.getbuffer(blackimage1), epd.getbuffer(redimage1))
     
-    logging.info("Clear...")
-    epd.init()
-    epd.Clear()
+    # logging.info("Clear...")
+    # epd.init()
+    # epd.Clear()
     
     logging.info("Goto Sleep...")
     epd.sleep()
